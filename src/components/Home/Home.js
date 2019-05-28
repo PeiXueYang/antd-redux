a26da4aea9bd94c545bdd3e897c79958f6b77cbe
PeiXueYang <32354744+PeiXueYang@users.noreply.github.com>
2019-05-28 15:11:51 +0800

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,48 +1,82 @@
 import React,{Component} from 'react';
 import './Home.css'
-import store from '../../store/store'
 import {getCommentList} from '../../api/api'
 import { connect } from 'react-redux'
+import { Button } from 'antd';
 class Home extends Component {
 	constructor(props){
 		super(props)
-	
-    this.state = {  }
+	 
+    this.state = { 
+			  comments:{}
+		 }
+	}
+	test(index){
+		console.log(index)
 	}
 	componentDidMount(){
+	 console.log(this.props)
+//  this.props.dispatch({
+// 				type:'EDIT',
+// 				payload:'测试一下对不对'
+	// 	       })
+	let {dispatch} = this.props
 		getCommentList().then(res=>{
-			store.dispatch( {
+			dispatch( {
 				type:'success',
 				payload: res.data
 			})
 		})
+
+	
 	}
     render() {
 		const {comments} = this.props.comments
 		const lists =	comments == null ?'': comments.map((item,index)=>{
-					return (
-									<div key={index}> 
-											<div>id: {item.id} </div>
-											<div>用户名: {item.name} </div>
-											<div> 邮箱:{item.emil} </div>
-											<div>内容: 
-												<div className='content'>	{item.body} </div>	
-											</div>
-									</div>
-						 )
-				})
+			return (
+				<div key={index}> 
+						<div>id: {item.id} </div>
+						<div>用户名: {item.name} </div>
+						<div> 邮箱:{item.email} </div>
+						<div>内容: 
+							<div className='content'>	{item.body} </div>	
+						</div>
+				</div>
+			    )
+		     })
         return (
             <div className='home'>
-							 <h1>Home组件</h1>
-							 <div>{lists}</div>
-						</div>
+		 <h1>Home组件</h1>
+		 <Button type='primary' onClick={this.test.bind(this,123)}>  点我试下 </Button>
+		 <div>{lists}</div>
+	</div>
         );
     }
 }
 const mapStateToProps = (state, ownProps) => {
+	// console.log(ownProps,'ownProps')
   return {
-    comments: state.comments
-  }
+		comments: state.comments,
+	}
 }
+// 通过 mapDispatchToProps 可以进行事件的派发
+// 通过派发一个事情  从而可以改变 state中的 值
+// const mapDispatchToProps =( dispatch )=>{
+//    return {
+// 		postListReducer:dispatch({
+// 			type:'EDIT',
+// 			payload:'teststs'
+// 		})
+// 	 }
+
+// }
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+	return {dispatch}
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
+// const map
 // 通过connect连接组件和redux数据,传递state数据和dispatch方法
-export default connect(mapStateToProps)(Home);
+// export default connect(mapStateToProps,mapDispatchToProps)(Home);
